44a785ec0b9be45ac80362ae91fa8d0fb2d7510b
Pablo David Garaguso <pablo.garaguso@gmail.com>
2022-10-26 23:30:55 +0300

    Chapter 5 ready

---
+++ chapter 5/OTP example/SignInOTP.vue
@@ -0,0 +1,77 @@
+<script setup>
+/**
+    A basic example of the front end needed to implement an email OTP
+    workflow
+ */
+import axios from "axios"
+import { ref } from "vue"
+
+const
+    _user_email = ref(""),
+    _wait_seconds = ref(0),
+    _show_input_code = ref(false),
+    _otp_code = ref("")
+
+function signInUser() {
+    axios
+        .post("https://requestOTP_url", { email: _user_email.value })
+        .then(result => {
+            _wait_seconds.value = result.data.wait_time;
+            _show_input_code.value = true;
+            startOTPtimer();
+        }).catch(err => {
+            console.log(err.message)
+        })
+}
+
+function startOTPtimer() {
+    let interval_id = setInterval(() => {
+        if (_wait_seconds.value > 0) { 
+            _wait_seconds.value--; 
+        }else { 
+            clearInterval(interval_id); 
+        }
+    }, 1000)
+}
+
+function checkOTP() {
+    axios.post("https://validateOTP_URL",{code:_otp_code.value})
+   .then(result => {
+        if (result.status > 200 && result.status < 300) {
+            // User validated, proceed to protected route 
+            // for example, using $router.push(...)
+        } else {
+            // Validation failed. Redirect to error route 
+        }
+    }).catch(err => {
+         console.log(err)
+    }) 
+} 
+</script>
+
+<template>
+<section>
+
+    <!-- Email input -->
+    <div v-show="!_show_input_code">
+        <input type="email" v-model="_user_email">
+        <button @click="signInUser()">Submit</button>
+    </div>
+
+    <!-- Show code form -->
+    <div v-show="_show_input_code">
+        <input type="text" v-model="_otp_code">
+        <button @click="checkOTP()">Submit</button>
+        <hr>
+        <p>
+            You have {{_wait_seconds}} seconds before the code expires.
+        </p>
+    </div>
+
+</section>
+
+</template>
+
+<style scoped>
+
+</style>
