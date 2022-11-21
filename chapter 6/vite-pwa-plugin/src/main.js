d864cc84bb4e9c12657a4776ab0cea660b8b2540
Pablo David Garaguso <pablo.garaguso@gmail.com>
2022-11-21 05:10:22 +0200

    Chapter 6 ready

--- chapter 6/vite-pwa-plugin/src/main.js
+++ chapter 6/vite-pwa-plugin/src/main.js
@@ -2,9 +2,4 @@ import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
 
-// Check if browser supports service workers
-if(navigator.serviceWorker){
-    navigator.serviceWorker.register("/service_worker.js");
-}
-
 createApp(App).mount('#app')
--- chapter 6/vite-pwa-plugin/vite.config.js
+++ chapter 6/vite-pwa-plugin/vite.config.js
@@ -3,5 +3,43 @@ import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [vue(), VitePWA({registerType:"autoUpdate"})]
+    plugins: [
+        vue(),
+        VitePWA({
+            registerType: "autoUpdate",
+            injectRegister: 'auto',
+            devOptions: { enabled: true },
+            workbox: {
+                globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+            },
+            includeAssets: ['fonts/*.ttf', 'images/*.png', 'css/*.css'],
+            manifest: {
+                "short_name": "PWA Example",
+                "name": "Chapter 6 - Progressive Web Application Example",
+                "start_url": "/",
+                "display": "standalone",
+                "theme_color": "#333333",
+                "background_color": "#000000",
+                "orientation": "portrait",
+                "icons": [
+                    {
+                        "src": "/images/chapter_6_icon_192x192.png",
+                        "sizes": "192x192",
+                        "type": "image/png"
+                    },
+                    {
+                        "src": "/images/chapter_6_icon.png",
+                        "sizes": "512x512",
+                        "type": "image/png"
+                    },
+                    {
+                        "src": "/images/chapter_6_icon.png",
+                        "sizes": "512x512",
+                        "type": "image/png",
+                        "purpose":"maskable"
+                    }
+                ],
+                "prefer_related_applications": false
+            }
+        })]
 })
