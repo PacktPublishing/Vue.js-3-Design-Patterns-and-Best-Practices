9fbb7173b5f341328dc416a97eb63a02c72c38bc
Pablo David Garaguso <pablo.garaguso@gmail.com>
2022-11-21 03:44:20 +0200

    Manual upscale SPA to PWA

---
+++ chapter 6/vite-pwa-plugin/vite.config.js
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
+
+export default defineConfig({
+  plugins: [vue(), VitePWA({registerType:"autoUpdate"})]
+})
