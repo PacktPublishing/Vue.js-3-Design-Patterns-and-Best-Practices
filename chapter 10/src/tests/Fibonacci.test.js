2064dfa3b442de8e1fe684ada154c9002177db0b
Pablo David Garaguso <pablo.garaguso@gmail.com>
2023-02-25 17:56:48 +0200

    Chapter 10 ready

--- chapter 10/src/tests/Fibonacci.test.js
+++ chapter 10/src/tests/Fibonacci.test.js
@@ -2,14 +2,17 @@ import { describe, expect, test } from "vitest"
 import { Fibonacci, FibonacciPromise } from "../services/Fibonacci.js"
 
 
-describe("Self contained function", () => {
+describe("Test the results in fibonacci()", () => {
 
-    test("Numbers in range", ()=>{
+    test("Results according to the series definition", ()=>{
         
+        // Expected values as defined by the series
         expect(Fibonacci(0)).toBe(0)
         expect(Fibonacci(1)).toBe(1)
         expect(Fibonacci(2)).toBe(1)
         expect(Fibonacci(3)).toBe(2)
+
+        // A value defined by the calculation of the series
         expect(Fibonacci(10)).toBe(55)
 
     })
@@ -17,11 +20,15 @@ describe("Self contained function", () => {
 
 describe("No negative numbers", () => {
     test("Out of range, must fail and throw an error", ()=>{
+
+        // The series is not defined for negative numbers, so it must throw an error.
         expect(()=>Fibonacci(-5)).toThrow()
     })
 })
 
 describe("Check async function", ()=>{
+    // For testing asynchronouse functions, the testing function should be async 
+    // and we "await" the entire "expect" resolution
     test("Resolve promise", async ()=>{
         await expect(FibonacciPromise(10)).resolves.toBe(55)
     })
--- chapter 10/vite.config.js
+++ chapter 10/vite.config.js
@@ -5,6 +5,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   test:{
-    environment:"jsdom"
+    environment:"jsdom",
+    coverage: {
+      reporter: ['default','text', 'json', 'html'],
+    }
   }
 })
