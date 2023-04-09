2d935df5c287be0d226998084a7acc94a603e5d9
Pablo David Garaguso <pablo.garaguso@gmail.com>
2023-04-09 14:47:48 +0300

    Corrected typos

--- chapter 4/todo-list-example/src/components/ToDoList.vue
+++ chapter 4/todo-list-example/src/components/ToDoList.vue
@@ -35,21 +35,21 @@ function emitEvent(event_name, payload) {
             </thead>
             <tbody>
                 <tr v-for="item in _filtered_list" :key="item.id">
-                    <td class="clickable w3-hover-pale-blue " @click="emitEvent("toggle", item)">
-                        <i class="fa-solid fa-2x fa-square w3-text-light-gray" v-if="item.status=="not_started""></i>
-                        <i class="fa-solid fa-2x fa-square-minus w3-text-teal" v-if="item.status=="in_progress""></i>
-                        <i class="fa-solid fa-2x fa-square-check w3-text-green" v-if="item.status=="completed""></i>
+                    <td class="clickable w3-hover-pale-blue " @click="emitEvent('toggle', item)">
+                        <i class="fa-solid fa-2x fa-square w3-text-light-gray" v-if="item.status=='not_started'"></i>
+                        <i class="fa-solid fa-2x fa-square-minus w3-text-teal" v-if="item.status=='in_progress'"></i>
+                        <i class="fa-solid fa-2x fa-square-check w3-text-green" v-if="item.status=='completed'"></i>
                     </td>
                     <td>{{item.text}}</td>
                     <td class="w3-right-align">
                         <span class="clickable w3-transparent w3-text-indigo w3-hover-text-blue w3-hover-white w3-margin-right"
-                            @click="emitEvent("edit",item)">
+                            @click="emitEvent('edit',item)">
                             <i class="fa-solid fa-pen-to-square"></i>
                             Edit
                         </span>
                         
                         <span class="clickable w3-transparent w3-text-purple w3-hover-text-red w3-hover-white"
-                            @click="emitEvent("delete",item)">
+                            @click="emitEvent('delete',item)">
                             <i class="fa-solid fa-trash-can"></i>
                             Delete
                         </span>
--- chapter 4/todo-list-example/src/main.js
+++ chapter 4/todo-list-example/src/main.js
@@ -1,5 +1,4 @@
 import { createApp } from "vue"
-import "./style.css"
 import App from "./App.vue"
 import Modals from "./plugins/modals"
 import styles from "./assets/styles.css"
